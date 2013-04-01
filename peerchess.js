67f97129206f50126038bcfc64a65db374c9e2f9
Matthias Lohr <matthias@lohr.me>
2013-04-01 12:24:10 +0200

    open connection to peer

--- peerchess.js
+++ peerchess.js
@@ -9,24 +9,72 @@ var myPeerApiKey = 'byzn0pwgnt2csor';
 /**********************************************************************************************************************/
 // brain
 
+var GAME_STATUS_LOADING = 1; // loading/initializing and connecting to peer server
+var GAME_STATUS_INITIALIZED = 2; // waiting for remote connection
+var GAME_STATUS_RUNNING = 3; // active game
+var GAME_STATUS_FINISHED = 4;
+
 var PeerChessGame = Class.create({
 	initialize: function(options, callbacks) {
 		this.callbacks = callbacks;
+		this.gameStatus = GAME_STATUS_LOADING;
 		this.peer = new Peer({
-			'key': options.peerApiKey
+			'key': options.peerApiKey,
+			'debug': true
 		});
 		var that = this;
 		this.peer.on('open', function(peerId) {
+			that.gameStatus = GAME_STATUS_INITIALIZED;
 			that.executeCallback('onInitialized', {'peerId': peerId});
 		});
-		this.peer.on('connection', function(connection) {
+		this.peer.on('close', function() {
 
 		});
+		this.peer.on('error', function (error) {
+
+		});
+		this.peer.on('connection', function(connection) {
+			connection.on('open', function() {
+				if (that.gameStatus == GAME_STATUS_INITIALIZED) {
+					// that.peer.disconnect(); // TODO waiting for implementation of this function
+					that.gameStatus = GAME_STATUS_RUNNING;
+					that.connection = connection;
+					that.connection.on('data', function(data) {
+						that.parseIncomingData(data);
+					});
+				}
+			});
+		});
+	},
+
+	connectToPeer: function(peerId) {
+		if (this.gameStatus !== GAME_STATUS_INITIALIZED) return false;
+		var connection = this.peer.connect(peerId);
+		var that = this;
+		connection.on('open', function() {
+			if (that.gameStatus == GAME_STATUS_INITIALIZED) {
+				// that.peer.disconnect(); // TODO waiting for implementation of this function
+				that.gameStatus = GAME_STATUS_RUNNING;
+				that.connection = connection;
+				that.connection.on('data', function(data) {
+					that.parseIncomingData(data);
+				});
+			}
+		});
+
 	},
 
 	executeCallback: function(callbackName, options) {
 		if (this.callbacks[callbackName] === undefined) return undefined;
 		return this.callbacks[callbackName].call(this, options);
+	},
+
+	getGameStatus: function() {
+		return this.gameStatus;
+	},
+
+	parseIncomingData: function(data) {
+
 	}
 });
 
@@ -90,11 +138,18 @@ var PawnFigure = Class.create(PeerChessFigure, {
 
 // interaction stuff
 document.observe("dom:loaded", function() {
+	// init game
 	var game = new PeerChessGame({
 		peerApiKey: myPeerApiKey
 	}, {
 		onInitialized: function(data) {
 			$('local-id').update(data.peerId);
+		}
+	});
+
+	$('remote-id').observe('keyup', function(event) {
+		if (event.keyCode == 13) {
+			game.connectToPeer($('remote-id').getValue());
 		}
 	});
 });
